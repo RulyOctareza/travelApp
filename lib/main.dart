ec9f986b9d7c8e7f07b8ea782c282fcc315e26b0
RulyOctareza <rulyoctareza@gmail.com>
2024-05-25 11:11:22 +0900

    add firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:travel_app/cubit/page_cubit.dart';
+import 'package:travel_app/firebase_options.dart';
 import 'package:travel_app/ui/pages/bonus_page.dart';
 import 'package:travel_app/ui/pages/get_started_page.dart';
 import 'package:travel_app/ui/pages/main_page.dart';
 import 'package:travel_app/ui/pages/sign_up_page.dart';
 import 'package:travel_app/ui/pages/splash_page.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
